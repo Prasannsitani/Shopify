c5e53adc4123398cf60d6eeb56796e3f7ed47bde
Prasann Sitani <prasannsitani000@gmail.com>
2021-03-18 00:39:18 +0530

    Top Sale and Categories section

---
+++ index.js
@@ -0,0 +1,10 @@
+// alert(123);
+// var $grid = $(".grid").isotype({
+//     itemSelector: '.grid-item',
+//     layoutMode : 'fitRows'
+// });
+
+// $(".button-group").on('click', "button", function() {
+//     var filterValue = $(this).attr("data-filter");
+//     $grid.isotype({filter: filterValue});
+// })
